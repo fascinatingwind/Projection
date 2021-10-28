d8e12efe1940bb6f2f58f4d622b62d9544b8f5f6
Ermek <fascwind@gmail.com>
2021-10-28 14:30:37 +0500

    add flow

--- Projection/Source.cpp
+++ Projection/Source.cpp
@@ -1,10 +1,15 @@
 #include <cstdlib>
-#include <iostream>
 
 #include "FileReader.h"
 #include "StringConverter.h"
+#include "FPoint3D.h"
+#include "FPoint3DParser.h"
+#include "ConsolePrint.h"
+#include "ProjectionFinder.h"
+#include "SolutionStore.h"
 
 using namespace Projection;
+using namespace SpaceX;
 
 std::string GetString(char* argv)
 {
@@ -13,24 +18,31 @@ std::string GetString(char* argv)
 
 int main(int argc, char* argv[])
 {
+#pragma region ParseArgs
 	if (argc < 3)
 	{
-		std::cerr << "Please enter file name with vertex coords and point of projection." << std::endl;
+		ConsolePrint::Print("Please enter file name with vertex coords and point of projection.");
 		return EXIT_SUCCESS;
 	}
 
 	const auto file_name = GetString(argv[1]);
 
-	std::vector<std::string> point_values;
+	std::string point_values;
 	for (int i = 2; i < argc; i++) {
-		point_values.push_back(GetString(argv[i]));
+		point_values += GetString(argv[i]) + " ";
 	}
+#pragma endregion
 
 	FileReader reader(file_name);
 	const auto lines = reader.GetLines();
 
-	const auto point_coord = StringConverter::ConvertAll(point_values);
-	const auto values = StringConverter::ConvertAll(lines);
+	const auto point_coord = FPoint3DParser::GetPoint(point_values);
+	const auto points = FPoint3DParser::GetPoints(lines);
 
+	const auto solutions = ProjectionFinder::FindNearPointsProjection(
+		points, point_coord);
+
+	for(const auto& sol : solutions)
+		ConsolePrint::PrintResults(sol);
 	return EXIT_SUCCESS;
 }
