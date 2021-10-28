daa27cec161b6b4576e7d439632c71e07c396e47
Ermek <fascwind@gmail.com>
2021-10-28 12:27:49 +0500

    add method for find projection solution

--- Projection/ProjectionFinder.cpp
+++ Projection/ProjectionFinder.cpp
@@ -1,13 +1,43 @@
 #include "ProjectionFinder.h"
 
 #include "KramerProjectionSolver.h"
+#include "LambdaParameter.h"
 
 namespace Projection
 {
-    SolutionStore ProjectionFinder::FindNearPointProjection(const std::vector<FPoint3D> poly_line, 
-        const FPoint3D& input_point)
+    SolutionStore ProjectionFinder::FindNearPointProjection(
+        const FLine3D& line, const FPoint3D& input_point)
     {
-        SolutionStore solution;
-        return solution;
+        SolutionStore store;
+        KramerProjectionSolver solver;
+        store.m_projection = solver.CalculateProjection(line, input_point);
+        store.m_lambda_parameter = LambdaParameter::CalculateLambda(line, store.m_projection);
+        
+        // if lambda param is out of bound [0;1] than we take line points
+        if (NumericComparer::IsGreater(store.m_lambda_parameter, 1.f))
+        {
+            store.m_projection = line.GetEndPoint();
+        }
+        else if (NumericComparer::IsLess(store.m_lambda_parameter, 0.f))
+        {
+            store.m_projection = line.GetStartPoint();
+        }
+
+        return store;
+    }
+    std::vector<SolutionStore> ProjectionFinder::FindNearPointsProjection(
+        const std::vector<FPoint3D> poly_line, const FPoint3D& input_point)
+    {
+        std::vector<SolutionStore> solutions;
+        KramerProjectionSolver solver;
+        for (int i = 1; i < poly_line.size(); i++)
+        {
+            auto solution = FindNearPointProjection(
+                FLine3D(poly_line[i - 1], poly_line[i]), input_point);
+            solution.m_segment_number = i - 1;
+            solutions.push_back(solution);
+        }
+
+        return solutions;
     }
 }
--- Projection/ProjectionFinder.h
+++ Projection/ProjectionFinder.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "FPoint3D.h"
+#include "FLine3D.h"
 #include "SolutionStore.h"
 
 namespace Projection
@@ -14,6 +15,9 @@ namespace Projection
 		~ProjectionFinder() = delete;
 
 		static SolutionStore FindNearPointProjection(
+			const FLine3D& line, const FPoint3D& input_point);
+
+		static std::vector<SolutionStore> FindNearPointsProjection(
 			const std::vector<FPoint3D> poly_line, const FPoint3D& input_point);
 	};
 }
