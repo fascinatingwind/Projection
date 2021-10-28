857aa1713ebbfcdfa5de8314c2cd35c7b05ff332
Ermek <fascwind@gmail.com>
2021-10-28 14:30:15 +0500

    change argument

--- Projection/ProjectionFinder.cpp
+++ Projection/ProjectionFinder.cpp
@@ -11,7 +11,7 @@ namespace Projection
         SolutionStore store;
         KramerProjectionSolver solver;
         store.m_projection = solver.CalculateProjection(line, input_point);
-        store.m_lambda_parameter = LambdaParameter::CalculateLambda(line, store.m_projection);
+        store.m_lambda_parameter = LambdaParameter::CalculateLambda(line, input_point);
         
         // if lambda param is out of bound [0;1] than we take line points
         if (NumericComparer::IsGreater(store.m_lambda_parameter, 1.f))
@@ -25,17 +25,20 @@ namespace Projection
 
         return store;
     }
+
     std::vector<SolutionStore> ProjectionFinder::FindNearPointsProjection(
         const std::vector<FPoint3D> poly_line, const FPoint3D& input_point)
     {
         std::vector<SolutionStore> solutions;
-        KramerProjectionSolver solver;
+        solutions.reserve(poly_line.size());
+
         for (int i = 1; i < poly_line.size(); i++)
         {
             auto solution = FindNearPointProjection(
                 FLine3D(poly_line[i - 1], poly_line[i]), input_point);
             solution.m_segment_number = i - 1;
-            solutions.push_back(solution);
+            if(solution.m_lambda_parameter >= 0 && solution.m_lambda_parameter <= 1)
+                solutions.emplace_back(solution);
         }
 
         return solutions;
