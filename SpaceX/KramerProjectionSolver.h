ae09f6ca817bd4a05b1dd92290e3d1b9b73c8e4c
Ermek <fascwind@gmail.com>
2021-10-29 15:44:52 +0500

    add comments

--- SpaceX/KramerProjectionSolver.h
+++ SpaceX/KramerProjectionSolver.h
@@ -7,12 +7,14 @@
 
 namespace SpaceX
 {
+	// Store method for find projection point
 	class SpaceX_ExportDll KramerProjectionSolver 
 	{
 	public:
 		KramerProjectionSolver() = default;
 		~KramerProjectionSolver() = default;
 
+		// Find projection point using Kramer's method for solve SLE
 		FPoint3D CalculateProjection(const FLine3D& line, const FPoint3D& point) const;
 	};
 }
