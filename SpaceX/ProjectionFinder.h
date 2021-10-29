c99b29bcfc922ea39fd8ed436b7443ea039b8227
Ermek <fascwind@gmail.com>
2021-10-29 15:32:07 +0500

    add comments

--- SpaceX/ProjectionFinder.h
+++ SpaceX/ProjectionFinder.h
@@ -7,15 +7,18 @@
 
 namespace SpaceX
 {
+	// Contain methods for find projection point for poly line
 	class SpaceX_ExportDll ProjectionFinder
 	{
 	public:
 		ProjectionFinder() = delete;
 		~ProjectionFinder() = delete;
 
+		// Returns the projection point of a line segment.
 		static SolutionStore FindNearPointProjection(
 			const FLine3D& line, const FPoint3D& input_point);
 
+		// Returns all projection points of a poly line
 		static std::vector<SolutionStore> FindNearPointsProjection(
 			const std::vector<FPoint3D>& poly_line, const FPoint3D& input_point);
 	};
